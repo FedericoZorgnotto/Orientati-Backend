bf73f69a95f5a17751555f5a72a4f9a7654fbad9
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-09-27 21:13:05 +0200

    Aggiunto versionamento API

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ from pydantic_settings import SettingsConfigDict, BaseSettings
 class Settings(BaseSettings):
     app_name: str = "Vallauri orientamento"
     database_url: str
+    version: str
     secret_key: str
     model_config = SettingsConfigDict(env_file=".env")
 
--- app/main.py
+++ app/main.py
@@ -1,13 +1,41 @@
+from importlib.metadata import version
+
 from fastapi import FastAPI
 # from app.routers.v1 import auth, users
 from app.config import settings
+from fastapi_versioning import VersionedFastAPI, version
 
-app = FastAPI()
+
+description = """
+This is the API for the Vallauri orientamento project.
+# Root 
+The API root is located at `/`, it responses with a welcome message.
+## Authentication
+The API uses JWT for authentication. You can obtain a token by sending a POST request to `/api/v1/auth/login`.
+
+
+"""
+
+app = FastAPI(
+    title=settings.app_name,
+    description=description,
+    version=settings.version
+
+)
 
 
 # app.include_router(auth.router, prefix="/api/v1")
 # app.include_router(users.router, prefix="/api/v1")
 
 @app.get("/")
-def read_root():
+@version(1, 0)
+async def read_root():
+    """
+    This is the root path of the API
+
+    It will return a welcome message
+    """
     return {"message": f"Welcome to {settings.app_name}"}
+
+
+app = VersionedFastAPI(app, version_format='{major}', prefix_format='/api/v{major}')
