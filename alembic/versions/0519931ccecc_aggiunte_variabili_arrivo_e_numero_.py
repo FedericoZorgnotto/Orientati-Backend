50338929b766eac1daccab39aea62f86f9bbbb0b
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-10-14 11:19:47 +0200

    bugfix librerie

--- alembic/versions/0519931ccecc_aggiunte_variabili_arrivo_e_numero_.py
+++ alembic/versions/0519931ccecc_aggiunte_variabili_arrivo_e_numero_.py
@@ -7,9 +7,8 @@ Create Date: 2024-10-10 18:01:26.723687
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = '0519931ccecc'
