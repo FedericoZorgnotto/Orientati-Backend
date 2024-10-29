11735da6905571e2e325ff1a0e341f366829e2f1
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-10-29 17:50:38 +0100

    aggiunte api percorsoDiStudi

--- app/routers/v1/admin/admin.py
+++ app/routers/v1/admin/admin.py
@@ -3,14 +3,16 @@ from fastapi import APIRouter
 from .indirizzi import indirizzi_router
 from .orientati import orientati_router
 from .orientatori import orientatori_router
+from .percorsiDiStudi import percorsiDiStudi_router
 from .utenti import utenti_router
 
 router = APIRouter()
 
+router.include_router(indirizzi_router, prefix="/indirizzi")
+router.include_router(percorsiDiStudi_router, prefix="/percorsiDiStudi")
 router.include_router(utenti_router, prefix="/utenti")
 router.include_router(orientatori_router, prefix="/orientatori")
 router.include_router(orientati_router, prefix="/orientati")
-router.include_router(indirizzi_router, prefix="/indirizzi")
 
 
 @router.get("/")
