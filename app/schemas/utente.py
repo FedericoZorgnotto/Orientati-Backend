f251a249e6f8879ec5b40686fbefe6837e0da6f2
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-11-15 11:15:58 +0100

    bugfix admin in update utente

--- app/schemas/utente.py
+++ app/schemas/utente.py
@@ -36,7 +36,7 @@ class UserCreate(UserBase):
 class UserUpdate(BaseModel):
     username: Optional[str] = None
     password: Optional[str] = None
-    admin: Optional[int] = None
+    admin: Optional[bool] = None
     temporaneo: Optional[bool] = None
     orientatore_id: Optional[int] = None
 
