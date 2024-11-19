f4abd0500a0a09b0fced7d6676db0dd57b29dc84
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-11-19 22:28:35 +0100

    aggiunta modifica presenza orientati

--- app/routers/v1/admin/dashboard/orientati.py
+++ app/routers/v1/admin/dashboard/orientati.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 
 from app.database import get_db
 from app.middlewares.auth_middleware import admin_access
-from app.models import Gruppo
+from app.models import Gruppo, Orientato, Presente
 from app.schemas.dashboard.orientato import OrientatoList, OrientatoBase
 
 orientati_router = APIRouter()
@@ -28,10 +28,11 @@ async def get_all_orientati(db: Session = Depends(get_db), _=Depends(admin_acces
                     break
 
             orientati.append(OrientatoBase(
+                id=orientato.id,
                 nome=orientato.nome,
                 cognome=orientato.cognome,
                 scuolaDiProvenienza_nome=orientato.scuolaDiProvenienza.nome,
-                presente= presente
+                presente=presente
             ))
 
     # ordinamento per presenza, prima quelli assenti
@@ -39,3 +40,27 @@ async def get_all_orientati(db: Session = Depends(get_db), _=Depends(admin_acces
 
     return OrientatoList(orientati=orientati)
 
+
+@orientati_router.put("/{orientato_id}")
+async def update_orientato(orientato_id: int, presente: bool, db: Session = Depends(get_db), _=Depends(admin_access)):
+    """
+    Segna un orientato come presente o assente al primo gruppo di oggi
+    """
+    orientato = db.query(Orientato).filter(Orientato.id == orientato_id).first()
+    if not orientato:
+        raise HTTPException(status_code=404, detail="Orientato not found")
+
+    gruppo = db.query(Gruppo).filter(Gruppo.data == datetime.now().strftime("%d/%m/%Y")).first()
+    if not gruppo:
+        raise HTTPException(status_code=404, detail="Gruppo not found")
+
+    if presente:
+        gruppo.presenti.append(Presente(orientato_id=orientato_id))
+    else:
+        for orientatoPresente in gruppo.presenti:
+            if orientatoPresente.orientato_id == orientato_id:
+                db.delete(orientatoPresente)
+                break
+
+    db.commit()
+    return {"message": "Orientato updated successfully"}
--- app/schemas/dashboard/orientato.py
+++ app/schemas/dashboard/orientato.py
@@ -4,15 +4,11 @@ from pydantic import BaseModel, ConfigDict
 
 
 class OrientatoBase(BaseModel):
+    id: int
     nome: str
     cognome: str
     scuolaDiProvenienza_nome: str
     presente: Optional[bool] = None
-
-
-class OrientatoResponse(OrientatoBase):
-    id: int
-
 
 class OrientatoList(BaseModel):
     orientati: List[OrientatoBase]
