ff4fa733ae1ec92bfbd2fe2b31a437be99d10185
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2024-11-19 19:55:59 +0100

    aggiunti nomi orientatori in bashboard admin gruppo

--- app/routers/v1/admin/dashboard/gruppi.py
+++ app/routers/v1/admin/dashboard/gruppi.py
@@ -16,5 +16,14 @@ async def get_all_gruppi(db: Session = Depends(get_db), _=Depends(admin_access))
     """
     Legge tutti i gruppi del giorno dal database
     """
-    gruppi = db.query(Gruppo).filter(Gruppo.data == datetime.now().strftime("%Y-%m-%d")).all()
-    return GruppoList(gruppi=[GruppoResponse.model_validate(gruppo) for gruppo in gruppi]) # TODO: aggiungere i nomi degli orientatori
+    gruppi = db.query(Gruppo).filter(Gruppo.data == datetime.now().strftime("%d/%m/%Y")).all()
+    listaGruppi = GruppoList(gruppi=[])
+    listaGruppi.gruppi = [GruppoResponse.model_validate(gruppo) for gruppo in gruppi]
+    for gruppo in listaGruppi.gruppi:
+        gruppo.nomi_orientatori = []
+        orientatori = db.query(Gruppo).filter(Gruppo.id == gruppo.id).all()
+        for orientatore in orientatori:
+            gruppo.nomi_orientatori.append(orientatore.nome)
+
+    listaGruppi.gruppi = sorted(listaGruppi.gruppi, key=lambda gruppo: gruppo.orario_partenza)
+    return listaGruppi
