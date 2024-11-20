3765d042c4a1af29ead816fcd2706e78767af18a
Pietro <p.audifreddi.2619@vallauri.edu>
2024-11-20 19:14:53 +0100

    Bugfix controlli api, aggiunti campi in tappa orientatore

---
+++ app/schemas/OrientatoreSchema/tappa.py
@@ -0,0 +1,22 @@
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+class TappaBase(BaseModel):
+    percorso_id: int
+    aula_id: int
+    minuti_arrivo: int
+    minuti_partenza: int
+    aula_nome: str
+    aula_posizione: str
+    aula_materia: str
+
+
+class TappaResponse(TappaBase):
+    id: int
+
+
+
+class TappaList(BaseModel):
+    tappe: list[TappaResponse]
