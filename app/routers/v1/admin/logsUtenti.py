3f62c5f00479e7a15fc0a29d68d90846550b44ae
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-02-12 13:00:39 +0100

    fix: rimmosi try superflui nelle api logs

--- app/routers/v1/admin/logsUtenti.py
+++ app/routers/v1/admin/logsUtenti.py
@@ -15,43 +15,33 @@ async def get_all_logs(_=Depends(admin_access)):
     """
     Legge tutti i log dal database
     """
-    try:
-        database = get_mongodb()
-        logs_collection = database.get_collection(settings.MONGODB_LOGS_COLLECTION)
-        logs = await logs_collection.find().sort("timestamp", -1).to_list()
-        return LogUtenteList(logs=[LogUtenteResponse(
-            id=str(log['_id']),
-            timestamp=log['timestamp'].isoformat(),
-            utente_id=log['utente_id'],
-            categoria=log['categoria'],
-            azione=log['azione'],
-            client_ip=log['client_ip'],
-            dati=json.dumps(log['dati'])
-        ) for log in logs])
-
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
-
+    database = get_mongodb()
+    logs_collection = database.get_collection(settings.MONGODB_LOGS_COLLECTION)
+    logs = await logs_collection.find().sort("timestamp", -1).to_list()
+    return LogUtenteList(logs=[LogUtenteResponse(
+        id=str(log['_id']),
+        timestamp=log['timestamp'].isoformat(),
+        utente_id=log['utente_id'],
+        categoria=log['categoria'],
+        azione=log['azione'],
+        client_ip=log['client_ip'],
+        dati=json.dumps(log['dati'])
+    ) for log in logs])
 
 @logsUtenti_router.get("/{utente_id}", response_model=LogUtenteList)
 async def get_utente_logs(utente_id: int, _=Depends(admin_access)):
     """
     Legge tutti i log appartenenti ad un utente dal database
     """
-    try:
-        database = get_mongodb()
-        logs_collection = database.get_collection(settings.MONGODB_LOGS_COLLECTION)
-        logs = logs_collection.find({"utente_id": utente_id}).sort("timestamp", -1)
-        return LogUtenteList(logs=[LogUtenteResponse(
-            id=str(log['_id']),
-            timestamp=log['timestamp'].isoformat(),
-            utente_id=log['utente_id'],
-            categoria=log['categoria'],
-            azione=log['azione'],
-            client_ip=log['client_ip'],
-            dati=json.dumps(log['dati'])
-        ) async for log in logs])
-    except Exception as e:
-        print(e)
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Internal server error")
+    database = get_mongodb()
+    logs_collection = database.get_collection(settings.MONGODB_LOGS_COLLECTION)
+    logs = logs_collection.find({"utente_id": utente_id}).sort("timestamp", -1)
+    return LogUtenteList(logs=[LogUtenteResponse(
+        id=str(log['_id']),
+        timestamp=log['timestamp'].isoformat(),
+        utente_id=log['utente_id'],
+        categoria=log['categoria'],
+        azione=log['azione'],
+        client_ip=log['client_ip'],
+        dati=json.dumps(log['dati'])
+    ) async for log in logs])
