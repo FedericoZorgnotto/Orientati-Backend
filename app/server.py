92eed4ba13edb6bbbcc86430ff9c17b6b2ec34f8
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-04-02 10:03:21 +0200

    update: momentaneamente rimosso logging

--- app/server.py
+++ app/server.py
@@ -53,63 +53,63 @@ async def read_root():
 app = VersionedFastAPI(app, version_format='{major}', prefix_format='/api/v{major}')
 
 
-@app.middleware("http")
-async def log_user_action_middleware(request: Request, call_next):
-    db = next(get_db())
-
-    # recupera l'utente loggato dal token JWT
-    user_id = None
-    if "Authorization" in request.headers and request.headers["Authorization"].startswith("Bearer "):
-        try:
-            token = request.headers["Authorization"].split("Bearer ")[1]
-            decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.algorithm])
-            user_id = db.query(Utente).filter(Utente.username == decoded_token["sub"]).first().id
-        except JWTError:
-            pass
-
-    # Aggiunge log pre-richiesta (facoltativo)
-    start_time = datetime.datetime.now(pytz.timezone("Europe/Rome"))
-
-    # Read the request body
-    dati_input = await request.body()
-    request._body = dati_input  # Store the body in the request object
-
-    # Processa la richiesta
-    response = await call_next(request)
-
-    # Aggiunge log post-richiesta
-    end_time = datetime.datetime.now(pytz.timezone("Europe/Rome"))
-    elapsed_time = (end_time - start_time).total_seconds()
-    dati_input = dati_input.decode("utf-8", errors="ignore")
-
-    # Leggi il corpo della risposta
-    body = b""
-    async for chunk in response.body_iterator:
-        body += chunk
-    dati_output = body.decode("utf-8")
-
-    client_ip = request.client.host
-
-    # Loggare l'azione
-    if user_id:
-        await log_user_action(
-            utente_id=int(user_id),
-            azione=f"Accessed {request.url.path}",
-            categoria=CategoriaLogUtente.INFO,
-            client_ip=client_ip,
-            dati={"method": request.method, "status_code": response.status_code, "request_code": dati_input,
-                  "request_output": dati_output, "elapsed_time": elapsed_time},
-        )
-    else:
-        await log_user_action(
-            azione=f"Accessed {request.url.path}",
-            categoria=CategoriaLogUtente.INFO,
-            client_ip=client_ip,
-            dati={"method": request.method, "status_code": response.status_code, "request_code": dati_input,
-                  "request_output": dati_output, "elapsed_time": elapsed_time},
-        )
-    return StreamingResponse(iter([body]), status_code=response.status_code, headers=dict(response.headers))
-
+# @app.middleware("http")
+# async def log_user_action_middleware(request: Request, call_next):
+#     db = next(get_db())
+#
+#     # recupera l'utente loggato dal token JWT
+#     user_id = None
+#     if "Authorization" in request.headers and request.headers["Authorization"].startswith("Bearer "):
+#         try:
+#             token = request.headers["Authorization"].split("Bearer ")[1]
+#             decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.algorithm])
+#             user_id = db.query(Utente).filter(Utente.username == decoded_token["sub"]).first().id
+#         except JWTError:
+#             pass
+#
+#     # Aggiunge log pre-richiesta (facoltativo)
+#     start_time = datetime.datetime.now(pytz.timezone("Europe/Rome"))
+#
+#     # Read the request body
+#     dati_input = await request.body()
+#     request._body = dati_input  # Store the body in the request object
+#
+#     # Processa la richiesta
+#     response = await call_next(request)
+#
+#     # Aggiunge log post-richiesta
+#     end_time = datetime.datetime.now(pytz.timezone("Europe/Rome"))
+#     elapsed_time = (end_time - start_time).total_seconds()
+#     dati_input = dati_input.decode("utf-8", errors="ignore")
+#
+#     # Leggi il corpo della risposta
+#     body = b""
+#     async for chunk in response.body_iterator:
+#         body += chunk
+#     dati_output = body.decode("utf-8")
+#
+#     client_ip = request.client.host
+#
+#     # Loggare l'azione
+#     if user_id:
+#         await log_user_action(
+#             utente_id=int(user_id),
+#             azione=f"Accessed {request.url.path}",
+#             categoria=CategoriaLogUtente.INFO,
+#             client_ip=client_ip,
+#             dati={"method": request.method, "status_code": response.status_code, "request_code": dati_input,
+#                   "request_output": dati_output, "elapsed_time": elapsed_time},
+#         )
+#     else:
+#         await log_user_action(
+#             azione=f"Accessed {request.url.path}",
+#             categoria=CategoriaLogUtente.INFO,
+#             client_ip=client_ip,
+#             dati={"method": request.method, "status_code": response.status_code, "request_code": dati_input,
+#                   "request_output": dati_output, "elapsed_time": elapsed_time},
+#         )
+#     return StreamingResponse(iter([body]), status_code=response.status_code, headers=dict(response.headers))
+#
 
 @app.middleware("http")
 async def cors_handler(request: Request, call_next):
