4f35af029815bb6fb75595fd3d2974898be04401
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-04-06 09:46:35 +0200

    feat: aggiunta struttura base service orientatore

---
+++ app/services/orientatore/gruppo.py
@@ -0,0 +1,24 @@
+def get_gruppo_utente(current_user):
+    """
+    restituisce il gruppo al quale l'utente è connesso
+    """
+    pass
+
+def get_tappe_gruppo(gruppi_id):
+    """
+    restituisce le tappe del gruppo
+    """
+    pass
+
+def get_tappa_gruppo(gruppo_id, numero_tappa):
+    """
+    restituisce la tappa N del gruppo
+    """
+    pass
+
+def imposta_tappa_gruppo(gruppo_id, numero_tappa, arrivato):
+    """
+    imposta la tappa del gruppo
+    """
+    pass
+
