6ad1aec80e3c78ae22502b2382ee900bf2a8f0cb
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-04-15 09:18:23 +0200

    feat: aggiunta api get scuole di provenienza

--- app/routers/v1/public/public.py
+++ app/routers/v1/public/public.py
@@ -3,10 +3,11 @@ from fastapi import APIRouter
 from .date import date_router
 from .genitore import genitore_router
 from .ragazzo import ragazzo_router
+from .scuolaDiProvenienza import scuola_router
 
 router = APIRouter()
 
 router.include_router(date_router, prefix="/date", tags=["Date"])
 router.include_router(genitore_router, prefix="/genitore", tags=["Genitore"])
-
 router.include_router(ragazzo_router, prefix="/ragazzo", tags=["Ragazzo"])
+router.include_router(scuola_router, prefix="/scuola", tags=["Scuola"])
