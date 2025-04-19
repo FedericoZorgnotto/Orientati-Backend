41642986ce0f0c0a0b0235e8d6cc98d6511a51f6
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-04-19 20:58:13 +0200

    feat: aggiunta api creazione iscrizione

--- app/routers/v1/public/iscrizione.py
+++ app/routers/v1/public/iscrizione.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
+from app.database import get_db
 from app.middlewares.auth_middleware import genitoreRegistrato_access, admin_access
-from app.schemas.iscrizione import IscrizioneList
-from app.services.public.iscrizione import iscrizioni_genitore, iscrizioni_all
+from app.models import Gruppo, FasciaOraria, Ragazzo
+from app.schemas.iscrizione import IscrizioneList, Iscrizione, IscrizioneCreate
+from app.services.public.iscrizione import iscrizioni_genitore, iscrizioni_all, create_iscrizione
 
 iscrizione_router = APIRouter()
 
@@ -27,3 +29,35 @@ async def read_all_iscrizioni(admin=Depends(admin_access)):
     if not iscrizioni:
         return {"iscrizioni": []}
     return {"iscrizioni": iscrizioni}
+
+
+@iscrizione_router.post("/", response_model=Iscrizione, summary="Crea una nuova iscrizione")
+async def create_iscrizione_endpoint(
+        iscrizione_data: IscrizioneCreate,
+        genitore=Depends(genitoreRegistrato_access),
+        db=Depends(get_db)
+):
+    """
+    Crea una nuova iscrizione per il genitore autenticato
+    """
+    # controlla se gruppo e fasciaOraria esistono nel database
+    gruppo = db.query(Gruppo).filter(Gruppo.id == iscrizione_data.gruppo_id).first()
+    if not gruppo:
+        raise HTTPException(status_code=404, detail="Gruppo non trovato")
+    fasciaOraria = db.query(FasciaOraria).filter(FasciaOraria.id == iscrizione_data.fasciaOraria_id).first()
+    if not fasciaOraria:
+        return HTTPException(status_code=404, detail="FasciaOraria non trovata")
+
+    # controlla se i ragazzi esistono nel database
+    for ragazzo_id in iscrizione_data.ragazzi_id:
+        ragazzo = db.query(Ragazzo).filter(Ragazzo.id == ragazzo_id).first()
+        if not ragazzo:
+            raise HTTPException(status_code=404, detail=f"Ragazzo con ID {ragazzo_id} non trovato")
+
+    iscrizione = create_iscrizione(
+        genitore_id=genitore.id,
+        gruppo_id=iscrizione_data.gruppo_id,
+        fasciaOraria_id=iscrizione_data.fasciaOraria_id,
+        ragazzi_id=iscrizione_data.ragazzi_id
+    )
+    return iscrizione
--- app/services/public/iscrizione.py
+++ app/services/public/iscrizione.py
@@ -1,4 +1,7 @@
+from sqlalchemy.orm import joinedload
+
 from app.database import get_db
+from app.models import Ragazzo
 from app.models.iscrizione import Iscrizione
 
 
@@ -22,3 +25,24 @@ def iscrizioni_all():
     if not iscrizioni:
         return None
     return iscrizioni
+
+
+def create_iscrizione(genitore_id: int, gruppo_id: int, fasciaOraria_id: int, ragazzi_id: list[int]):
+    """
+    Create a new registration for a parent.
+    """
+    database = next(get_db())
+    iscrizione = Iscrizione(
+        genitore_id=genitore_id,
+        gruppo_id=gruppo_id,
+        fasciaOraria_id=fasciaOraria_id
+    )
+
+    ragazzi = database.query(Ragazzo).filter(Ragazzo.id.in_(ragazzi_id)).all()
+    iscrizione.ragazzi = ragazzi
+
+    database.add(iscrizione)
+    database.commit()
+    database.refresh(iscrizione)
+    iscrizione.ragazzi = ragazzi
+    return iscrizione
