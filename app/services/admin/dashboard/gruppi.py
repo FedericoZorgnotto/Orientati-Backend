330f354c6f3b73b2dc2a737dfcf36c28fbc24bf5
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-05-30 17:42:20 +0200

    fix: corretta funzione get_all_gruppi

--- app/services/admin/dashboard/gruppi.py
+++ app/services/admin/dashboard/gruppi.py
@@ -1,5 +1,5 @@
 from app.database import get_db
-from app.models import Gruppo
+from app.models import Gruppo, Presente, Assente
 from app.schemas.admin.dashboard.gruppo import GruppoList, GruppoResponse
 
 
@@ -9,10 +9,10 @@ def get_all_gruppi():
     """
     db = next(get_db())
     # gruppi = db.query(Gruppo).filter(Gruppo.data == datetime.now().strftime("%d/%m/%Y")).all()
-    gruppi = db.query(Gruppo).all()
+    gruppi = db.query(Gruppo).join(Gruppo.fasciaOraria).all()
+    # ordino i gruppi per fascia oraria
+    gruppi = sorted(gruppi, key=lambda gruppo: gruppo.fasciaOraria.oraInizio)
     listaGruppi = GruppoList(gruppi=[])
-    print(gruppi)
-    print (not gruppi)
     if not gruppi:
         return listaGruppi
 
@@ -26,21 +26,27 @@ def get_all_gruppi():
         if not gruppo.numero_tappa == 0:
             db_gruppo = db.query(Gruppo).filter(Gruppo.id == gruppo.id).first()
 
-            tappe = sorted(db_gruppo.percorso.tappe, key=lambda tappa: tappa.minuti_partenza)
+            tappe = sorted(db_gruppo.fasciaOraria.percorso.tappe, key=lambda tappa: tappa.minuti_partenza)
+            if gruppo.numero_tappa == 0:
+                gruppo.aula_nome = ""
+                gruppo.aula_posizione = ""
+                gruppo.aula_materia = ""
+                gruppo.minuti_arrivo = 0
+                gruppo.minuti_partenza = 0
+            else:
+                gruppo.aula_nome = tappe[gruppo.numero_tappa - 1].aula.nome
+                gruppo.aula_posizione = tappe[gruppo.numero_tappa - 1].aula.posizione
+                gruppo.aula_materia = tappe[gruppo.numero_tappa - 1].aula.materia
+                gruppo.minuti_arrivo = tappe[gruppo.numero_tappa - 1].minuti_arrivo
+                gruppo.minuti_partenza = tappe[gruppo.numero_tappa - 1].minuti_partenza
 
-            gruppo.aula_nome = tappe[gruppo.numero_tappa - 1].aula.nome
-            gruppo.aula_posizione = tappe[gruppo.numero_tappa - 1].aula.posizione
-            gruppo.aula_materia = tappe[gruppo.numero_tappa - 1].aula.materia
-            gruppo.minuti_arrivo = tappe[gruppo.numero_tappa - 1].minuti_arrivo
-            gruppo.minuti_partenza = tappe[gruppo.numero_tappa - 1].minuti_partenza
+        iscrizioni = db.query(Gruppo).filter(Gruppo.id == gruppo.id).first().iscrizioni
+        ragazzi = [ragazzo for iscrizione in iscrizioni for ragazzo in iscrizione.ragazzi]
+        gruppo.totale_orientati = len(ragazzi)
+        presenti = db.query(Presente).filter(Presente.gruppo_id == gruppo.id).all()
+        gruppo.orientati_presenti = len(presenti)
+        assenti = db.query(Assente).filter(Assente.gruppo_id == gruppo.id).all()
+        gruppo.orientati_assenti = len(assenti)
 
-        # orientati = db.query(Gruppo).filter(Gruppo.id == gruppo.id).first().orientati
-        # gruppo.totale_orientati = len(orientati)
-        # presenti = db.query(Presente).filter(Presente.gruppo_id == gruppo.id).all()
-        # gruppo.orientati_presenti = len(presenti)
-        # assenti = db.query(Assente).filter(Assente.gruppo_id == gruppo.id).all()
-        # gruppo.orientati_assenti = len(assenti)
-
-    listaGruppi.gruppi = sorted(listaGruppi.gruppi, key=lambda gruppo: gruppo.orario_partenza)
     listaGruppi.gruppi = sorted(listaGruppi.gruppi, key=lambda gruppo: gruppo.percorsoFinito is True)
     return listaGruppi
