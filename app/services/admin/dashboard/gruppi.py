a11736acd4ac77e0fa9de23acc2599dd267bd55d
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-05-30 16:51:48 +0200

    fix: get all gruppi quando la tabella è vuota

--- app/services/admin/dashboard/gruppi.py
+++ app/services/admin/dashboard/gruppi.py
@@ -11,6 +11,8 @@ def get_all_gruppi():
     # gruppi = db.query(Gruppo).filter(Gruppo.data == datetime.now().strftime("%d/%m/%Y")).all()
     gruppi = db.query(Gruppo).all()
     listaGruppi = GruppoList(gruppi=[])
+    if not gruppi:
+        return listaGruppi
 
     listaGruppi.gruppi = [GruppoResponse.model_validate(gruppo) for gruppo in gruppi]
     for gruppo in listaGruppi.gruppi:
