a6d597119e8ab84e735cc9d93f912585f38dd0c6
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-06-06 21:11:49 +0200

    fix: corretta coerenza nome comandi ws

--- app/websoket/manager.py
+++ app/websoket/manager.py
@@ -49,23 +49,23 @@ class WebSocketManager:
                     await websocket.close()
                     logger.info(f"Disconnessione richiesta da {user.role}: {user.user.id}")
 
-                elif message_type == "update_groups":
+                elif message_type == "reload_groups":
                     if user.role == UserRole.ADMIN_DASHBOARD:
                         await invia_admin_gruppi(websocket)
                     else:
                         logger.warning(f"Utente {user.user.id} non autorizzato a richiedere i gruppi")
-                elif message_type == "update_orientati":
+                elif message_type == "reload_orientati":
                     if user.role == UserRole.ADMIN_DASHBOARD:
                         await invia_admin_orientati(websocket)
                     else:
                         logger.warning(f"Utente {user.user.id} non autorizzato a richiedere gli orientati")
-                elif message_type == "update_aule":
+                elif message_type == "reload_aule":
                     if user.role == UserRole.ADMIN_DASHBOARD:
                         await invia_admin_aule(websocket)
                     else:
                         logger.warning(f"Utente {user.user.id} non autorizzato a richiedere le aule")
 
-                elif message_type == "update_user_group":
+                elif message_type == "reload_user_group":
                     if user.role == UserRole.USER:
                         await invia_user_gruppo(user, websocket)
                     else:
