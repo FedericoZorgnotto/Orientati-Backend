70b180720411c6688b54b4e0022fd8e07e45cfa6
Federico Zorgnotto <f.zorgnotto.2759@vallauri.edu>
2025-06-10 18:55:22 +0200

    update: spostato get_genitori in service
    implementato contextmanager per get_db

---
+++ app/services/admin/dashboard/genitori.py
@@ -0,0 +1,10 @@
+from app.database import get_db_context
+from app.models import Genitore
+
+
+def get_all_genitori():
+    with get_db_context() as db:
+        genitori = db.query(Genitore).distinct().all()
+        genitori_list = [{"id": genitore.id, "nome": genitore.nome, "cognome": genitore.cognome} for genitore in genitori if
+                         genitore is not None]
+        return genitori_list
--- app/websoket/dashboard/services.py
+++ app/websoket/dashboard/services.py
@@ -4,8 +4,8 @@ import logging
 from fastapi import WebSocket
 
 from ...database import get_db
-from ...models import Iscrizione, Ragazzo, ScuolaDiProvenienza, Genitore
-from ...services.admin.dashboard import gruppi, scuoleDiProvenienza
+from ...models import Iscrizione, Ragazzo
+from ...services.admin.dashboard import gruppi as Gruppi, scuoleDiProvenienza as ScuoleDiProvenienza, genitori as Genitori
 from ...services.admin.dashboard.aule import get_all_aule
 from ...services.admin.dashboard.orientati import get_all_orientati
 
@@ -79,7 +79,7 @@ async def invia_admin_gruppi(websocket: WebSocket, percorso_id: int = None):
                 "orientati_assenti": g.orientati_assenti,
                 "orario_partenza": g.orario_partenza,
                 "id": g.id
-            } for g in gruppi.get_all_gruppi(percorso_id=percorso_id).gruppi
+            } for g in Gruppi.get_all_gruppi(percorso_id=percorso_id).gruppi
         ]}))
 
 
@@ -88,9 +88,9 @@ async def genera_codice_gruppo(websocket: WebSocket, gruppo_id: int):
         await websocket.send_text(json.dumps({
             "type": "codice_gruppo",
             "gruppo_id": gruppo_id,
-            "codice": gruppi.genera_codice_gruppo(gruppo_id)
+            "codice": Gruppi.genera_codice_gruppo(gruppo_id)
         }))
-    except gruppi.GruppoNotFoundError as e:
+    except Gruppi.GruppoNotFoundError as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -106,9 +106,9 @@ async def invia_utenti_gruppo(websocket: WebSocket, gruppo_id: int):
                 "id": u.id,
                 "username": u.username,
                 "temporaneo": u.temporaneo,
-            } for u in gruppi.get_utenti_gruppo(gruppo_id) if u is not None]
+            } for u in Gruppi.get_utenti_gruppo(gruppo_id) if u is not None]
         }))
-    except gruppi.GruppoNotFoundError as e:
+    except Gruppi.GruppoNotFoundError as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -117,9 +117,9 @@ async def invia_utenti_gruppo(websocket: WebSocket, gruppo_id: int):
 
 async def rimuovi_utente_gruppo(websocket: WebSocket, user_id: int, group_id: int):
     try:
-        gruppi.rimuovi_utente(user_id, group_id)
+        Gruppi.rimuovi_utente(user_id, group_id)
         await invia_utenti_gruppo(websocket, group_id)
-    except (gruppi.GruppoNotFoundError, gruppi.UserNotFoundError) as e:
+    except (Gruppi.GruppoNotFoundError, Gruppi.UserNotFoundError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -128,14 +128,14 @@ async def rimuovi_utente_gruppo(websocket: WebSocket, user_id: int, group_id: in
 
 async def modifica_gruppo_iscrizione(websocket: WebSocket, group_id: int, iscrizione_id: int):
     try:
-        gruppi.modifica_gruppo_iscrizione(group_id, iscrizione_id)
+        Gruppi.modifica_gruppo_iscrizione(group_id, iscrizione_id)
         await websocket.send_text(json.dumps({
             "type": "iscrizione_modificata",
             "iscrizione_id": iscrizione_id,
             "gruppo_id": group_id,
             "message": "Iscrizione modificata con successo"
         }))
-    except (gruppi.GruppoNotFoundError, gruppi.IscrizioneNotFoundError) as e:
+    except (Gruppi.GruppoNotFoundError, Gruppi.IscrizioneNotFoundError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -144,14 +144,14 @@ async def modifica_gruppo_iscrizione(websocket: WebSocket, group_id: int, iscriz
 
 async def modifica_ragazzo_presente(websocket: WebSocket, ragazzo_id: int, group_id: int):
     try:
-        gruppi.modifica_ragazzo_presente(ragazzo_id, group_id)
+        Gruppi.modifica_ragazzo_presente(ragazzo_id, group_id)
         await websocket.send_text(json.dumps({
             "type": "ragazzo_presente",
             "user_id": ragazzo_id,
             "group_id": group_id,
             "message": "Ragazzo marcato come presente"
         }))
-    except (gruppi.RagazzoNotFoundError, gruppi.GruppoNotFoundError, gruppi.RagazzoAlreadyPresentError) as e:
+    except (Gruppi.RagazzoNotFoundError, Gruppi.GruppoNotFoundError, Gruppi.RagazzoAlreadyPresentError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -160,14 +160,14 @@ async def modifica_ragazzo_presente(websocket: WebSocket, ragazzo_id: int, group
 
 async def modifica_ragazzo_assente(websocket: WebSocket, ragazzo_id: int, group_id: int):
     try:
-        gruppi.modifica_ragazzo_assente(ragazzo_id, group_id)
+        Gruppi.modifica_ragazzo_assente(ragazzo_id, group_id)
         await websocket.send_text(json.dumps({
             "type": "ragazzo_assente",
             "user_id": ragazzo_id,
             "group_id": group_id,
             "message": "Ragazzo marcato come assente"
         }))
-    except (gruppi.RagazzoNotFoundError, gruppi.GruppoNotFoundError, gruppi.RagazzoAlreadyAbsentError) as e:
+    except (Gruppi.RagazzoNotFoundError, Gruppi.GruppoNotFoundError, Gruppi.RagazzoAlreadyAbsentError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -176,14 +176,14 @@ async def modifica_ragazzo_assente(websocket: WebSocket, ragazzo_id: int, group_
 
 async def modifica_ragazzo_non_arrivato(websocket: WebSocket, ragazzo_id: int, group_id: int):
     try:
-        gruppi.modifica_ragazzo_non_arrivato(ragazzo_id, group_id)
+        Gruppi.modifica_ragazzo_non_arrivato(ragazzo_id, group_id)
         await websocket.send_text(json.dumps({
             "type": "ragazzo_non_arrivato",
             "user_id": ragazzo_id,
             "group_id": group_id,
             "message": "Ragazzo marcato come non arrivato"
         }))
-    except (gruppi.RagazzoNotFoundError, gruppi.GruppoNotFoundError) as e:
+    except (Gruppi.RagazzoNotFoundError, Gruppi.GruppoNotFoundError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -192,14 +192,14 @@ async def modifica_ragazzo_non_arrivato(websocket: WebSocket, ragazzo_id: int, g
 
 async def modifica_fascia_oraria_orario_partenza(websocket: WebSocket, fascia_oraria_id: int, orario_partenza: str):
     try:
-        gruppi.modifica_fascia_oraria_orario_partenza(fascia_oraria_id, orario_partenza)
+        Gruppi.modifica_fascia_oraria_orario_partenza(fascia_oraria_id, orario_partenza)
         await websocket.send_text(json.dumps({
             "type": "fascia_oraria_modificata",
             "fascia_oraria_id": fascia_oraria_id,
             "orario_partenza": orario_partenza,
             "message": "Orario di partenza della fascia oraria modificato con successo"
         }))
-    except (gruppi.FasciaOrariaNotFoundError, gruppi.GruppoNotFoundError) as e:
+    except (Gruppi.FasciaOrariaNotFoundError, Gruppi.GruppoNotFoundError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -208,14 +208,14 @@ async def modifica_fascia_oraria_orario_partenza(websocket: WebSocket, fascia_or
 
 async def modifica_gruppo_nome(websocket: WebSocket, group_id: int, new_name: str):
     try:
-        gruppi.modifica_gruppo_nome(group_id, new_name)
+        Gruppi.modifica_gruppo_nome(group_id, new_name)
         await websocket.send_text(json.dumps({
             "type": "gruppo_modificato",
             "group_id": group_id,
             "new_name": new_name,
             "message": "Nome del gruppo modificato con successo"
         }))
-    except (gruppi.GruppoNotFoundError, gruppi.InvalidGroupNameError) as e:
+    except (Gruppi.GruppoNotFoundError, Gruppi.InvalidGroupNameError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -225,7 +225,7 @@ async def modifica_gruppo_nome(websocket: WebSocket, group_id: int, new_name: st
 async def modifica_gruppo_tappa(websocket: WebSocket, group_id: int, numero_tappa: int, arrivato: str):
     try:
         arrivato = arrivato.strip().lower() == "true"
-        gruppi.modifica_gruppo_tappa(group_id, numero_tappa, arrivato)
+        Gruppi.modifica_gruppo_tappa(group_id, numero_tappa, arrivato)
         await websocket.send_text(json.dumps({
             "type": "gruppo_tappa_modificata",
             "group_id": group_id,
@@ -233,7 +233,7 @@ async def modifica_gruppo_tappa(websocket: WebSocket, group_id: int, numero_tapp
             "arrivato": arrivato,
             "message": "Tappa del gruppo modificata con successo"
         }))
-    except (gruppi.GruppoNotFoundError, gruppi.InvalidTappaNumberError) as e:
+    except (Gruppi.GruppoNotFoundError, Gruppi.InvalidTappaNumberError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -243,7 +243,7 @@ async def modifica_gruppo_tappa(websocket: WebSocket, group_id: int, numero_tapp
 async def crea_ragazzo_gruppo(websocket: WebSocket, group_id: int, name: str, surname: str,
                               scuolaDiProvenienza_id: int = None, genitore_id: int = None):
     try:
-        ragazzo, iscrizione = gruppi.crea_ragazzo_gruppo(group_id, name, surname, scuolaDiProvenienza_id, genitore_id)
+        ragazzo, iscrizione = Gruppi.crea_ragazzo_gruppo(group_id, name, surname, scuolaDiProvenienza_id, genitore_id)
         await websocket.send_text(json.dumps({
             "type": "ragazzo_creato",
             "group_id": group_id,
@@ -254,7 +254,7 @@ async def crea_ragazzo_gruppo(websocket: WebSocket, group_id: int, name: str, su
             "iscrizione_id": iscrizione.id,
             "message": f"Ragazzo {name} {surname} creato e aggiunto al gruppo"
         }))
-    except (gruppi.GruppoNotFoundError, gruppi.InvalidRagazzoDataError) as e:
+    except (Gruppi.GruppoNotFoundError, Gruppi.InvalidRagazzoDataError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -264,7 +264,7 @@ async def crea_ragazzo_gruppo(websocket: WebSocket, group_id: int, name: str, su
 async def crea_ragazzo_iscrizione(websocket: WebSocket, iscrizione_id: int, name: str, surname: str,
                                   scuolaDiProvenienza_id: int = None):
     try:
-        ragazzo, iscrizione = gruppi.crea_ragazzo_iscrizione(iscrizione_id, name, surname, scuolaDiProvenienza_id)
+        ragazzo, iscrizione = Gruppi.crea_ragazzo_iscrizione(iscrizione_id, name, surname, scuolaDiProvenienza_id)
         await websocket.send_text(json.dumps({
             "type": "ragazzo_creato_iscrizione",
             "iscrizione_id": iscrizione.id,
@@ -273,7 +273,7 @@ async def crea_ragazzo_iscrizione(websocket: WebSocket, iscrizione_id: int, name
             "scuolaDiProvenienza_id": scuolaDiProvenienza_id if scuolaDiProvenienza_id else None,
             "message": f"Ragazzo {name} {surname} creato e aggiunto all'iscrizione"
         }))
-    except (gruppi.IscrizioneNotFoundError, gruppi.InvalidRagazzoDataError) as e:
+    except (Gruppi.IscrizioneNotFoundError, Gruppi.InvalidRagazzoDataError) as e:
         await websocket.send_text(json.dumps({
             "type": "error",
             "message": str(e)
@@ -281,7 +281,7 @@ async def crea_ragazzo_iscrizione(websocket: WebSocket, iscrizione_id: int, name
 
 
 async def get_scuole_di_provenienza(websocket: WebSocket):
-    scuole = scuoleDiProvenienza.get_all_scuole()
+    scuole = ScuoleDiProvenienza.get_all_scuole()
     scuole_list = [{"id": scuola.id, "nome": scuola.nome} for scuola in scuole if scuola is not None]
     await websocket.send_text(json.dumps({
         "type": "scuole_di_provenienza",
@@ -290,12 +290,12 @@ async def get_scuole_di_provenienza(websocket: WebSocket):
 
 
 async def get_genitori(websocket: WebSocket):
-    db = next(get_db())
-    genitori = db.query(Genitore).distinct().all()
-
-    genitori_list = [{"id": genitore.id, "nome": genitore.nome, "cognome": genitore.cognome} for genitore in genitori if
-                     genitore is not None]
-
+    genitori = Genitori.get_all_genitori()
+    genitori_list = [{
+        "id": genitore.id,
+        "nome": genitore.nome,
+        "cognome": genitore.cognome
+    } for genitore in genitori if genitore is not None]
     await websocket.send_text(json.dumps({
         "type": "genitori",
         "genitori": genitori_list
